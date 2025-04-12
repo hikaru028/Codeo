86e6026cc838db9a1c09080e91c1eef036371d08
whereishikaru <h.suzuki.028@gmail.com>
2025-04-12 23:58:22 +0900

    feat: implement end call button

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import { useRouter } from 'next/navigation';
 import { CallControls, CallingState, CallParticipantsList, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from '@stream-io/video-react-sdk';
-import { LayoutListIcon, LoaderIcon } from 'lucide-react';
+import { LayoutListIcon, LoaderIcon, UsersIcon } from 'lucide-react';
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu';
 import { Button } from './ui/button';
+import EndCallButton from './EndCallButton';
 
 type Props = {}
 
@@ -25,50 +26,63 @@ const MeetingRoom = (props: Props) => {
     }
 
     return (
-        <ResizablePanelGroup direction='horizontal'>
-            {/* Left side (video) */}
-            <ResizablePanel defaultSize={35} minSize={25} maxSize={100} className='relative'>
-                {/* Video layout */}
-                <div className='absolute inset-0'>
-                    {layout === "grid" ? <PaginatedGridLayout /> : <SpeakerLayout />}
+        <div className='h-[calc(100vh-4rem-1px)]'>
+            <ResizablePanelGroup direction='horizontal'>
+                {/* Left side (video) */}
+                <ResizablePanel defaultSize={35} minSize={25} maxSize={100} className='relative'>
+                    {/* Video layout */}
+                    <div className='absolute inset-0'>
+                        {layout === "grid" ? <PaginatedGridLayout /> : <SpeakerLayout />}
 
-                    {showParticipants && (
-                        <div className='absolute right-0 top-0 h-full w-[300px] bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
-                            <CallParticipantsList onClose={() => setShowParticipants(false)} />
-                        </div>
-                    )}
-                </div>
+                        {showParticipants && (
+                            <div className='absolute right-0 top-0 h-full w-[300px] bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
+                                <CallParticipantsList onClose={() => setShowParticipants(false)} />
+                            </div>
+                        )}
+                    </div>
 
-                {/* Video controls */}
-                <div className='absolute bottom-4 left-0 right-0'>
-                    <div className='flex flex-col items-center gap-4'>
-                        <div className='flex items-center gap-2 flex-wrap justify center px-4'>
-                            <CallControls onLeave={() => router.push("/")} />
+                    {/* Video controls */}
+                    <div className='absolute bottom-4 left-0 right-0'>
+                        <div className='flex flex-col items-center gap-4'>
+                            <div className='flex items-center gap-2 flex-wrap justify center px-4'>
+                                <CallControls onLeave={() => router.push("/")} />
 
-                            <div className='flex items-center gap-2'>
-                                <DropdownMenu>
-                                    <DropdownMenuTrigger asChild>
-                                        <Button variant='outline' size='icon' className='size-10'>
-                                            <LayoutListIcon className='size-4' />
-                                        </Button>
-                                    </DropdownMenuTrigger>
-                                    <DropdownMenuContent>
-                                        <DropdownMenuItem onClick={() => setLayout('grid')}>Grid view</DropdownMenuItem>
-                                        <DropdownMenuItem onClick={() => setLayout('speaker')}>Speaker view</DropdownMenuItem>
-                                    </DropdownMenuContent>
-                                </DropdownMenu>
+                                <div className='flex items-center gap-2'>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button variant='outline' size='icon' className='size-10'>
+                                                <LayoutListIcon className='size-4' />
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent>
+                                            <DropdownMenuItem onClick={() => setLayout('grid')}>Grid view</DropdownMenuItem>
+                                            <DropdownMenuItem onClick={() => setLayout('speaker')}>Speaker view</DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+
+                                    <Button
+                                        variant='outline'
+                                        size='icon'
+                                        className='size-10'
+                                        onClick={() => setShowParticipants(!showParticipants)}
+                                    >
+                                        <UsersIcon className='size-4' />
+                                    </Button>
+
+                                    <EndCallButton />
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </ResizablePanel>
+                </ResizablePanel>
 
-            <ResizableHandle withHandle />
+                <ResizableHandle withHandle />
 
-            <ResizablePanel defaultSize={65} minSize={25}>
-                <h1>code editor will go here</h1>
-            </ResizablePanel>
-        </ResizablePanelGroup>
+                <ResizablePanel defaultSize={65} minSize={25}>
+                    <h1>code editor will go here</h1>
+                </ResizablePanel>
+            </ResizablePanelGroup>
+        </div>
     )
 }
 
